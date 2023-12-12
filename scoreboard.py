95fb9fa1d1b56008c4a28f15ee355ca5dde0f30e
idirbylka <84841803+idirbylka@users.noreply.github.com>
2023-12-12 15:33:40 +0100

    Initial commit

---
+++ scoreboard.py
@@ -0,0 +1,11 @@
+# from turtle import Turtle
+#
+# class Scoreboard(Turtle):
+#     def __init__(self):
+#         super().__init__()
+#         self.score = []
+#
+#
+#     def score_display(self):
+#         new_score =
+#         new_score.write("Your Score: ", True, align="center")
